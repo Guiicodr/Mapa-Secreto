f0fc07120795add6c7c07e92f6a45497cc19eed8
Guilhermehriq <contatoguilhermehrp@gmail.com>
2026-08-17 16:36:42 -0300

    fix: adjust Navbar import casing for Linux compatibility

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/NavBar';
 import PlaceCard from '../components/PlaceCard';
 
 const LOCAIS = [
